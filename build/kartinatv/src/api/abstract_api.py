59e963673a9dcabc6875406b38bf16b8cc16ab2d
Alex Maystrenko <alexeytech@gmail.com>
2011-08-19 14:58:26 +0000

    import missed files
    git-svn-id: http://techhost.dlinkddns.com/svn/trunk@117 8e7748fa-d928-7d47-8ce4-b9025f518b90

---
+++ build/kartinatv/src/api/abstract_api.py
@@ -0,0 +1,32 @@
+#  Dreambox Enigma2 KartinaTV/RodnoeTV player! (by technic)
+#
+#  Copyright (c) 2010 Alex Maystrenko <alexeytech@gmail.com>
+#  web: http://techhost.dlinkddns.com/
+#
+# This is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2, or (at your option) any later
+# version.
+
+DEBUG = True
+MODE_STREAM = 0
+MODE_VIDEOS = 1
+
+class AbstractAPI:
+	
+	MODE = MODE_STREAM
+	iProvider = "free"
+	iName = "example"
+	NEXT_API = None 
+	
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+		self.SID = False
+	
+	def start(self):
+		pass
+	
+	def trace(self, msg):
+		if DEBUG:
+			print "[KartinaTV] %s: %s" % (self.iName, msg)
