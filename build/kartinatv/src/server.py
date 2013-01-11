3423e56a49ef7028f3142f5f637c2eb360683f57
Alex Maystrenko <alexeytech@gmail.com>
2013-01-11 21:45:21 +0200

    run stream redirect in thread
    fixes openpli playback in bouquets

--- build/kartinatv/src/server.py
+++ build/kartinatv/src/server.py
@@ -1,5 +1,5 @@
 from twisted.internet import reactor
-from twisted.web.server import Site
+from twisted.web.server import Site, NOT_DONE_YET
 from twisted.web.error import ErrorPage
 from twisted.python import urlpath
 from twisted.web import resource, util
@@ -9,17 +9,23 @@ from plugin import runManager
 class RedirectToStream(resource.Resource):
 	isLeaf = True
 	
-	def render(self, request):
+	def _threadedRender(self, request):
 		print "[KartinaTV] render"
 		req = request.path.split('/')
 		if len(req) == 3:
 			url = runManager.getStream(req[1], req[2], None)
 			if url:
 				print '[KartinaTV] server redirecting'
-				return util.redirectTo(url, request)
+				ret = util.redirectTo(url, request)
 			else:
-				return ErrorPage(404, 'api getStreamUrl failed', '').render(request)
+				ret = ErrorPage(404, 'api getStreamUrl failed', '').render(request)
 		else:
-			return ErrorPage(404, 'wrong request format', '').render(request)
+			ret = ErrorPage(404, 'wrong request format', '').render(request)
+		request.write(ret)
+		request.finish()
+	
+	def render(self, request):
+		reactor.callInThread(self._threadedRender, request)
+		return NOT_DONE_YET
 	
 reactor.listenTCP(9000, Site(RedirectToStream()))
